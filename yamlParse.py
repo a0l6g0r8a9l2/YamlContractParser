b521fbe3fae996b02deccaba6acb879c1c32e57f
Alex Gruzdev <a0l6g0r8a9l2@gmail.com>
2020-01-11 21:46:24 +0300

    Добавил поиск вх. параметров в parameters

--- yamlParse.py
+++ yamlParse.py
@@ -3,7 +3,8 @@ import pprint as pp
 from dataclasses import dataclass
 import pandas as pd
 
-# TODO: Сейчас выводятся обрбатываются вх. параметры. Переределать на сиходящие.
+
+# TODO: Сейчас выводятся обрбатываются вх. параметры. Добавить исходящие.
 #  Добавить в анализ словарь definitions -> responses (матчить из path)
 
 
@@ -41,35 +42,26 @@ class Params(Method):
     paramType: list
 
 
-subPaths = []
-methodNames = []
-pamNames = []
-pamTypes = []
-paramDescrip = []
+pathList = []
 for subPath, parsms in getDataContract()['paths'].items():
-    print("path: " + subPath)
-    subPaths.append(subPath)
+    # subPaths.append(subPath)  # путь до метода
     for item in parsms.values():
-        print("methodName: " + item['operationId'], end='\n')
-        methodNames.append(item['operationId'])
+        # print("methodName: " + item['operationId'], end='\n')  # метод
         for keys, vals in item.items():
             if keys == 'parameters':
                 for items in vals:
-                    if items['name']:  # items['type'], items['description']
-                        print(items['name'])
-                        pamNames.append(items['name'])
-                    elif items['type']:
-                        pamTypes.append(items['type'])
-                    elif items['description']:
-                        paramDescrip.append(items['description'])
-            # print(type(keys), type(vals), end='\n')
-            # print(keys, vals, end='\n')
-
+                    if 'schema' not in items.keys():  # если нет ключа "schema" значит. вх. параметры ищем в
+                        # parameters, инчае в "definitions"
+                        nameParamIn, typeParamIn = items['name'], items['type']  # вх. параметры
+                        # print(nameParamIn, typeParamIn, end='\n')
+                        itemPathList = (item['operationId'] + 'Request' + '/' + nameParamIn, typeParamIn)
+                        pathList.append(itemPathList)
+print(pathList)
 # df = pd.DataFrame(data) export_excel = df.to_excel(
 # r'C:\Users\Алексей\PycharmProjects\YamlContractParser\export_dataframe.xlsx', index=None, header=False)
 
 # TODO: добавить вывод в excel построчно
 
-parametrs = Params(getDataContract()['basePath'], path=subPaths, methodName=methodNames,
-                   paramName=pamNames, paramDiscrip=paramDescrip, paramType=pamTypes)
-print(parametrs.subPath + parametrs.path[1] + '/' + parametrs.paramName[1])
+# parametrs = Params(getDataContract()['basePath'], path=subPaths, methodName=methodNames,
+#                    paramName=pamNames, paramDiscrip=paramDescrip, paramType=pamTypes)
+# print(parametrs.subPath + parametrs.path[1] + '/' + parametrs.paramName[1])
