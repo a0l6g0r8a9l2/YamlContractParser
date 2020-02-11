4f434607860dc097c0eb0d70011815cfc9ff0042
Alex Gruzdev <a0l6g0r8a9l2@gmail.com>
2020-02-11 23:44:41 +0300

    Мелкие фиксы

--- tComonControler.py
+++ tComonControler.py
@@ -1,7 +1,5 @@
 #! C:\Users\Алексей\PycharmProjects\YamlContractParser\python-virtual-environments\env python
 import logging
-import urllib
-from urllib import request
 
 import requests
 import telebot
@@ -9,17 +7,12 @@ import yaml
 from telebot.types import Message
 
 import main
+from settings import API_TOKEN, proxies
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
 
-API_TOKEN = '864783318:AAG173kN2JGCSoGVil3Rld2OdV8csamdthI'
-# TODO: перенести в переменные окружения
-
 bot = telebot.TeleBot(API_TOKEN)
-proxies = {
-    'https': 'https://127.0.0.1:9080'
-}
 telebot.apihelper.proxy = proxies
 
 aboutBotText = 'Я - простой бот для парсинга YAML контрактов. \nОснованный на Swagger Open API Specification. ' \
@@ -38,19 +31,24 @@ def handle_start_help(message: Message):
     if message.content_type == 'document' and message.document.mime_type == 'application/x-yaml':
         document_id = message.document.file_id
         file_info = bot.get_file(document_id)
-        proxy = urllib.request.ProxyHandler({'http': 'https://127.0.0.1:9080'})
-        file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(API_TOKEN, file_info.file_path),
+        file = requests.get(f'https://api.telegram.org/file/bot{API_TOKEN}/{file_info.file_path}',
                             proxies=proxies, stream=True)
         data = yaml.safe_load(file.text)
         parsedData = main.getDataContract(data)
-        bot.reply_to(message, parsedData)
-        # TODO Исправить: в сообщении возвращается только первый элемент первого кортежжа из списка
+        i = 0
+        while i < len(parsedData):
+            if parsedData[i][1] is not None:
+                msg = parsedData[i][0] + ' <b>' + parsedData[i][1] + '</b>'
+            else:
+                msg = parsedData[i][0] + ' '
+            bot.send_message(message.chat.id, text=msg, parse_mode='HTML')
+            i += 1
     elif message.text == '/start':
         bot.reply_to(message, f'Привет, {user}! \n {startBotText}')
     elif message.text == '/help':
         bot.reply_to(message, f'{user}, вот что ты должен знать о моих возможностях: \n {helpBotText}')
     elif message.text == '/about':
-        bot.reply_to(message, f'{user}, вот что должен знать обо мне: \n {aboutBotText}')
+        bot.reply_to(message, f'{user}, вот что ты должен знать обо мне: \n {aboutBotText}')
 
 
 if __name__ == '__main__':
