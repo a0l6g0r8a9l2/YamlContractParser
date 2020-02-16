15e086bdd5e2fd3948855ae4e733ccec22be2fcd
Alex Gruzdev <a0l6g0r8a9l2@gmail.com>
2020-02-16 14:18:04 +0300

    Добавил: 1. кнопки с выбором типа параметров 2. сохранение последнего контракта из чата

--- tComonControler.py
+++ tComonControler.py
@@ -1,9 +1,11 @@
 #! C:\Users\Алексей\PycharmProjects\YamlContractParser\python-virtual-environments\env python
 import logging
+import pickle
 
 import requests
 import telebot
 import yaml
+from telebot import types
 from telebot.types import Message
 
 import main
@@ -32,31 +34,52 @@ msgTypes = {
 
 @bot.message_handler(commands=['start', 'help', 'about'])
 @bot.message_handler(content_types=['document'])
-def handle_start_help(message: Message):
-    user = message.from_user.username
+def common_doc_handler(message: Message):
     if message.content_type == 'document' and message.document.mime_type == 'application/x-yaml':
         document_id = message.document.file_id
         file_info = bot.get_file(document_id)
         file = requests.get(f'https://api.telegram.org/file/bot{API_TOKEN}/{file_info.file_path}',
-                            proxies=proxies, stream=True)
+                            proxies=proxies, stream=True, timeout=60)
         data = yaml.safe_load(file.text)
-        parsedData = main.getDataContract(data, context=msgTypes['Все параметры'])
-        i = 0
-        fullMsg = ''
-        while i < len(parsedData):
-            if parsedData[i][1] is not None:
-                msg = '*' + parsedData[i][0] + '*' + '    ' + '_' + parsedData[i][1] + '_'
-            else:
-                msg = '*' + parsedData[i][0] + '*'
-            i += 1
-            fullMsg = fullMsg + msg + '\n'
-        bot.reply_to(message, text=fullMsg, parse_mode='Markdown')
-    elif message.text == '/start':
+        with open(f'contract_{message.chat.id}.yaml', 'wb') as f:
+            pickle.dump(data, f)
+        markup = types.ReplyKeyboardMarkup(row_width=1)
+        itembtn1 = types.KeyboardButton(list(msgTypes.keys())[0])
+        itembtn2 = types.KeyboardButton(list(msgTypes.keys())[1])
+        itembtn3 = types.KeyboardButton(list(msgTypes.keys())[2])
+        markup.add(itembtn1, itembtn2, itembtn3)
+        bot.send_message(message.chat.id, "Выбирете тип параметров:", reply_markup=markup)
+
+
+@bot.message_handler(commands=['start', 'help', 'about'])
+def common_comand_handler(message: Message):
+    user = message.from_user.username
+    if message.text == '/start':
         bot.reply_to(message, f'Привет, {user}! \n {startBotText}')
     elif message.text == '/help':
         bot.reply_to(message, f'{user}, вот что ты должен знать о моих возможностях: \n {helpBotText}')
     elif message.text == '/about':
         bot.reply_to(message, f'{user}, вот что ты должен знать обо мне: \n {aboutBotText}')
+
+
+@bot.message_handler(func=lambda message: message.text in msgTypes.keys(), content_types=['text'])
+def common_result_handler(message):
+    with open(f'contract_{message.chat.id}.yaml', 'rb') as f:
+        data = pickle.load(f)
+    parsedData = main.getDataContract(data, context=msgTypes[message.text])
+    i = 0
+    fullMsg = ''
+    while i < len(parsedData):
+        if parsedData[i][1] is not None:
+            msg = '*' + parsedData[i][0] + '*' + '    ' + '_' + parsedData[i][1] + '_'
+        else:
+            msg = '*' + parsedData[i][0] + '*'
+        i += 1
+        fullMsg = fullMsg + msg + '\n'
+    if fullMsg != '':
+        bot.reply_to(message, text=fullMsg, parse_mode='Markdown')
+    else:
+        bot.reply_to(message, f'{message.text} отсутствуют')
 
 
 if __name__ == '__main__':
