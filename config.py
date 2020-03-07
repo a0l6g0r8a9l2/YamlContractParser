eb5a7958a7a1b36be952b82f77388b4635966a5f
Alex Gruzdev <a0l6g0r8a9l2@gmail.com>
2020-03-07 21:54:29 +0300

    доработки для деплоя _8

--- config.py
+++ config.py
@@ -2,8 +2,6 @@ import os
 import sys
 from logging import getLogger
 
-from settings import API_TOKEN, proxies
-
 logger = getLogger(__name__)
 
 
@@ -12,6 +10,7 @@ def load_config():
         conf_name = os.environ.get('BOT_ENV')
         logger.debug("Loaded config \"{}\" - OK".format(conf_name))
         if conf_name != 'prod':
+            from settings import API_TOKEN, proxies
             return API_TOKEN, proxies
         else:
             return os.environ.get('API_TOKEN'), os.environ.get('proxies')
